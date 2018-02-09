d7a8215d639c61b17457068f44245bb9cb91d491
iampaulmata <drake.mata@gmail.com>
2018-02-09 18:10:38 -0500

    This is the first submission of my work on this app.

--- Vicious Mockery/ViewController.swift
+++ Vicious Mockery/ViewController.swift
@@ -9,12 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var lblMockery: UILabel!
+    
+    @IBAction func btnPress(_ sender: UIButton) {
+        let array = ["You're stupid",
+                     "You're ugly",
+                     "I don't like you",
+                     "You're mamma dresses you funny"]
+        let randomIndex = Int(arc4random_uniform(UInt32(array.count)))
+        //lblMockery.text = "You pressed the button!"
+        lblMockery.text = (array[randomIndex])
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
